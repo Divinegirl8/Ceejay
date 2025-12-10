01dc1c3b9428c5f824345babfc2c6999cdf27184
Divinegirl8 <iamoluchimercy@gmail.com>
2025-12-10 07:56:22 +0100

    new commit

--- src/component/ResumeViewer.tsx
+++ src/component/ResumeViewer.tsx
@@ -1,7 +1,7 @@
 const ResumeViewer = () => {
     return (
       <iframe 
-        src="src/assets/Miti_Kotak_Resume.pdf"
+        src="/Miti_Kotak_Resume.pdf"
         style={{ width: "100%", height: "100vh", border: "none" }}
       />
     );
