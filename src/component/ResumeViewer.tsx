11fe9e03217946e8dffda9125cf866f1140838f1
Divinegirl8 <iamoluchimercy@gmail.com>
2026-02-09 14:22:41 +0100

    new commit

--- src/component/ResumeViewer.tsx
+++ src/component/ResumeViewer.tsx
@@ -1,7 +1,7 @@
 const ResumeViewer = () => {
     return (
       <iframe 
-        src="/Lucky_Ekezie_-_Lead_Product_Designer.pdf"
+        src="/Lucky_Ekezie_-_Senior_Product_Designer_—_AI_&_Complex_Systems copy.pdf"
         style={{ width: "100%", height: "100vh", border: "none" }}
       />
     );
